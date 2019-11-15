243af8d47c04a386ec767141e25171363f761e21
Brian Loveless <Brian.loveless1@yahoo.com>
2019-11-15 11:20:44 -0500

    app bones

---
+++ bl-simple/src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1> Simple > Complex</h1>
+      </header>
+      <nav>
+        <a className="link" href="#">
+          Home
+        </a>
+        <a className="link" href="#">
+          Weather
+        </a>
+        <a className="link" href="#">
+          Python
+        </a>
+        <a className="link" href="#">
+          How To
+        </a>
+        <a className="link" href="#">
+          JS Prompts
+        </a>
+        <a className="link" href="#">
+          JS Console
+        </a>
+      </nav>
+      <hr></hr>
+    </div>
+  );
+}
+
+export default App;
