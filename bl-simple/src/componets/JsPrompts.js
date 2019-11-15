e46f45197c525d06fa56a87a3cad107c37ddaff1
Brian Loveless <Brian.loveless1@yahoo.com>
2019-11-15 16:02:31 -0500

    js markdown live

--- bl-simple/src/componets/JsPrompts.js
+++ bl-simple/src/componets/JsPrompts.js
@@ -1,16 +1,18 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./JsPrompts.css";
+import "../../../simple-JavaScript/prompts/simple-prompts"
 
 class JsPrompts extends Component {
   render() {
     return (
       <div className="JsPromptsContainer">
         <h3 className="JsPrompts3">JsPrompts page here</h3>
+        <h3>On this page you can play the prompts version and access the original code to modify for your own game</h3>
         <Link className="code" to="/jsprompts/code">
           Click here for the prompts code 
         </Link>
-        
+        <button
       </div>
     );
   }
