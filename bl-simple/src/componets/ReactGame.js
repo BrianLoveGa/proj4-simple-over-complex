88e22746cf19711cd287ac404cae6a6a27f14fe9
Brian Loveless <Brian.loveless1@yahoo.com>
2019-12-07 19:06:42 -0500

    errors

--- bl-simple/src/componets/ReactGame.js
+++ bl-simple/src/componets/ReactGame.js
@@ -16,180 +16,183 @@ class ReactGame extends Component {
     };
   }
 
-  healthPotion = () => {
-    if (this.state.potions > 0) {
-      this.setState({
-        health: this.state.health + 25,
-        potions: this.state.potions - 1
-      });
-    } else {
-      alert("no more potions left");
-    }
-  };
+    healthPotion = () => {
+      if (this.state.potions > 0) {
+        this.setState({
+          health: this.state.health + 25,
+          potions: this.state.potions - 1
+        });
+      } else {
+        alert("no more potions left");
+      }
+    };
 
-  increaseNumber = () => {
-    if (this.state.number <= 20) {
-      this.setState({ number: this.state.number + 1 });
-    } else {
-      alert(
-        " YOU WIN ! \n yep it goes up to 21, \n ok thats far enough go back down now"
-      );
-    }
-  };
+    increaseNumber = () => {
+      if (this.state.number <= 20) {
+        this.setState({ number: this.state.number + 1 });
+      } else {
+        alert(
+          " YOU WIN ! \n yep it goes up to 21, \n ok thats far enough go back down now"
+        );
+      }
+    };
 
-  decreaseNumber = () => {
-    if (this.state.number >= -10) {
-      this.setState({ number: this.state.number - 1 });
-    } else {
-      alert(
-        " You lost ... kinda ... \n minus eleven is far enough  \n go back up now"
-      );
-    }
-  };
+    decreaseNumber = () => {
+      if (this.state.number >= -10) {
+        this.setState({ number: this.state.number - 1 });
+      } else {
+        alert(
+          " You lost ... kinda ... \n minus eleven is far enough  \n go back up now"
+        );
+      }
+    };
 
-  solve1 = () => {
-    this.setState({ solve: this.state.solve + 1 });
-  };
+    solve1 = () => {
+      this.setState({ solve: this.state.solve + 1 });
+    };
 
-  // lets say we want to add 10 attack for first  question solved and 20 for 2nd question
-  // and maybe just for fun 27
-  attackUp = () => {
-    if (this.state.solve === 0) {
-      this.setState({ attack: this.state.attack + 10 });
-    } else if (this.state.solve === 1) {
-      this.setState({ attack: this.state.attack + 20 });
-    } else {
-      this.setState({ attack: this.state.attack + 27 });
-    }
-  };
-  // lets add 10 health for each correct puzzle solve
-  healthUp = () => {
-    if (this.state.solve === 0) {
-      this.setState({ health: this.state.health + 15 });
-    } else if (this.state.solve === 1) {
-      this.setState({ health: this.state.health + 25 });
-    } else {
-      this.setState({ health: this.state.health + 50 });
-    }
-  };
-
-  badUp = () => {
-    this.setState({ badHealth: this.state.badHealth + 25 });
-  };
-
-  puzzle = () => {
-    if (this.state.health < 1) {
-      alert("sorry you are dead. \n allow me to reset the game for you");
-      this.reset();
-    } else if (this.state.badHealth < 1) {
-      alert(
-        "CONGRATS YOU DEFEATED THE ENEMY ! \n allow me to reset the game \n so you can play again"
-      );
-      this.reset();
-    } else {
+    // lets say we want to add 10 attack for first  question solved and 20 for 2nd question
+    // and maybe just for fun 27
+    attackUp = () => {
       if (this.state.solve === 0) {
-        this.puzzle1();
+        this.setState({ attack: this.state.attack + 10 });
       } else if (this.state.solve === 1) {
-        this.puzzle2();
-      } else if (this.state.solve === 2) {
-        this.puzzle3();
+        this.setState({ attack: this.state.attack + 20 });
       } else {
-        alert("sorry, only 3 puzzles here \n keeping it simple");
+        this.setState({ attack: this.state.attack + 27 });
       }
-    }
-  };
-
-  puzzle1 = () => {
-    let puz = prompt("What does 1 + 1 = ____?");
-    if (puz === null) {
-    } else {
-      if (puz === "2" || puz.toLowerCase() === "two") {
-        alert("correct answer good job");
-        this.solve1();
-        this.attackUp();
-        this.healthUp();
+    };
+    // lets add 10 health for each correct puzzle solve
+    healthUp = () => {
+      if (this.state.solve === 0) {
+        this.setState({ health: this.state.health + 15 });
+      } else if (this.state.solve === 1) {
+        this.setState({ health: this.state.health + 25 });
       } else {
-        alert("sorry that was not correct");
+        this.setState({ health: this.state.health + 50 });
       }
-    }
-  };
+    };
 
-  puzzle2 = () => {
-    let puz = prompt("Simple is better than complex? \n True or False?");
-    if (puz === null) {
-    } else {
-      if (puz.toLowerCase() === "true" || puz.toLowerCase() === "tru") {
-        alert("correct answer good job");
-        this.solve1();
-        this.attackUp();
-        this.healthUp();
+    badUp = () => {
+      this.setState({ badHealth: this.state.badHealth + 25 });
+    };
+
+    puzzle = () => {
+      if (this.state.health < 1) {
+        alert("sorry you are dead. \n allow me to reset the game for you");
+        this.reset();
+      } else if (this.state.badHealth < 1) {
+        alert(
+          "CONGRATS YOU DEFEATED THE ENEMY ! \n allow me to reset the game \n so you can play again"
+        );
+        this.reset();
       } else {
-        alert("sorry that was not correct");
+        if (this.state.solve === 0) {
+          this.puzzle1();
+        } else if (this.state.solve === 1) {
+          this.puzzle2();
+        } else if (this.state.solve === 2) {
+          this.puzzle3();
+        } else {
+          alert("sorry, only 3 puzzles here \n keeping it simple");
+        }
       }
-    }
-  };
+    };
 
-  puzzle3 = () => {
-    let puz = prompt("Can learning be fun? \n Yes or No ?");
-    if (puz === null) {
-    } else {
-      if (
-        puz.toLowerCase() === "yes" ||
-        puz.toLowerCase() === "certianly" ||
-        puz.toLowerCase() === "indeed"
-      ) {
-        alert("correct answer good job");
-        this.solve1();
-        this.attackUp();
-        this.healthUp();
-        this.badUp();
+    puzzle1 = () => {
+      let puz = prompt("What does 1 + 1 = ____?");
+      if (puz === null) {
       } else {
-        alert("sorry that was not correct");
+        if (puz === "2" || puz.toLowerCase() === "two") {
+          alert("correct answer good job");
+          this.solve1();
+          this.attackUp();
+          this.healthUp();
+        } else {
+          alert("sorry that was not correct");
+        }
       }
-    }
-  };
+    };
 
-  fight = () => {
-    alert("fight time \n Let the battle begin");
-    if (this.state.health < 0) {
-      alert("you can't fight you're dead \n reset the game to play again");
-    } else if (this.state.badHealth < 0) {
-      alert(
-        "CONGRATS YOU WIN THE ENEMY IS DEFEATED! \n reset then game to play again "
-      );
-    } else {
+    puzzle2 = () => {
+      let puz = prompt("Simple is better than complex? \n True or False?");
+      if (puz === null) {
+      } else {
+        if (puz.toLowerCase() === "true" || puz.toLowerCase() === "tru") {
+          alert("correct answer good job");
+          this.solve1();
+          this.attackUp();
+          this.healthUp();
+        } else {
+          alert("sorry that was not correct");
+        }
+      }
+    };
+
+    puzzle3 = () => {
+      let puz = prompt("Can learning be fun? \n Yes or No ?");
+      if (puz === null) {
+      } else {
+        if (
+          puz.toLowerCase() === "yes" ||
+          puz.toLowerCase() === "certianly" ||
+          puz.toLowerCase() === "indeed"
+        ) {
+          alert("correct answer good job");
+          this.solve1();
+          this.attackUp();
+          this.healthUp();
+          this.badUp();
+        } else {
+          alert("sorry that was not correct");
+        }
+      }
+    };
+
+    fight = () => {
+      alert("fight time \n Let the battle begin");
+      if (this.state.health < 0) {
+        alert("you can't fight you're dead \n reset the game to play again");
+      } else if (this.state.badHealth < 0) {
+        alert(
+          "CONGRATS YOU WIN THE ENEMY IS DEFEATED! \n reset then game to play again "
+        );
+      } else {
+        this.setState({
+          health: this.state.health - this.state.badAttack,
+          badHealth: this.state.badHealth - this.state.attack
+        });
+        alert(
+          "what a quick fight \n so fast I almost didn't even see you move"
+        );
+        if (this.state.health <= 1) {
+          alert("! SORRY ... game over ... \n you lost the fight");
+          alert("allow me to reset the game for you");
+          this.reset();
+        } else if (this.state.badHealth <= 1) {
+          alert("!!!!! congrats !!!!!! \n YOU WON THANKS FOR PLAYING !");
+          alert("allow me to reset the game for you");
+          this.reset();
+        }
+      }
+    };
+
+    reset = () => {
+      alert("Resetting the game to start data.");
+
+      /// this is not best practices but ok enough
+      ///  please ingnore the warnings for this simple game demo concept
       this.setState({
-        health: this.state.health - this.state.badAttack,
-        badHealth: this.state.badHealth - this.state.attack
+        number: (this.state.number = 0),
+        health: (this.state.health = 50),
+        solve: (this.state.solve = 0),
+        attack: (this.state.attack = 10),
+        potions: (this.state.potions = 2),
+        badAttack: (this.state.badAttack = 49),
+        badHealth: (this.state.badHealth = 50)
       });
-      alert("what a quick fight \n so fast I almost didn't even see you move");
-      if (this.state.health <= 1) {
-        alert("! SORRY ... game over ... \n you lost the fight");
-        alert("allow me to reset the game for you");
-        this.reset();
-      } else if (this.state.badHealth <= 1) {
-        alert("!!!!! congrats !!!!!! \n YOU WON THANKS FOR PLAYING !");
-        alert("allow me to reset the game for you");
-        this.reset();
-      }
-    }
-  };
-
-  reset = () => {
-    alert("Resetting the game to start data.");
-
-    /// this is not best practices but ok enough
-    ///  please ingnore the warnings for this simple game demo concept
-    this.setState({
-      number: (this.state.number = 0),
-      health: (this.state.health = 50),
-      solve: (this.state.solve = 0),
-      attack: (this.state.attack = 10),
-      potions: (this.state.potions = 2),
-      badAttack: (this.state.badAttack = 49),
-      badHealth: (this.state.badHealth = 50)
-    });
-  };
+    };
+  
 
   render() {
     return (
