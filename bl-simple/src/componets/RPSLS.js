1687ec3ec1243a42a8ee360bff86d7f6223153ad
Brian Loveless <Brian.loveless1@yahoo.com>
2020-03-01 18:58:51 -0500

    rpsls js

--- bl-simple/src/componets/RPSLS.js
+++ bl-simple/src/componets/RPSLS.js
@@ -23,6 +23,20 @@ export default class Rock extends Component {
         <h1>ROCK PAPER SCISSORS</h1>
         <h2>In Python</h2>
         <h3>With Lizard and Spock</h3>
+        <a
+          href="https://brianlovega.github.io/game-rock-paper-scissors/"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          JavaScript Version
+        </a>
+        <a
+          href="https://github.com/BrianLoveGa/game-rock-paper-scissors/blob/master/new.js"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          JS Code Link
+        </a>
         <h2>It doesn't get more simple than this</h2>
         <div className="mdcode">
           <ReactMarkdown source={markdown} />
